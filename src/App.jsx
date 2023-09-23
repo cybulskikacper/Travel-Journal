e5d91910fe49bf288d58dd770c171dad44958ca1
Kacper <kacpercybulski@icloud.com>
2023-09-23 15:01:08 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import data from './components/data'
+import './App.css'
+
+function App() {
+	return (
+		<>
+			<h4>hey you look nice today</h4>
+		</>
+	)
+}
+
+export default App
