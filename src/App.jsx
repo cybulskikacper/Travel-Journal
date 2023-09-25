3855d59f4302b436f854ce83b311b11caf767587
Kacper <kacpercybulski@icloud.com>
2023-09-25 15:04:33 +0200

    created nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import data from './components/data'
+import data from './components/Data'
+import Nav from './components/Nav'
 import './App.css'
 
 function App() {
 	return (
 		<>
-			<h4>hey you look nice today</h4>
+			<Nav />
 		</>
 	)
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Nav() {
+	return (
+		<nav>
+			<img className="icon" src="./images/Fill 213.png" />
+			<h1 className="title "> my travel journal.</h1>
+		</nav>
+	)
+}
+
+export default Nav
